8a31d369ba8cb1cebe2ab8bf914db4475fbace56
Kristoffer Sundin <79krisun@skola.boras.se>
2024-09-24 23:50:29 +0200

    Adds improvement to HomeScreen with image, SwitchToggle and styling

--- screens/HomeScreen.tsx
+++ screens/HomeScreen.tsx
@@ -1,9 +1,34 @@
+import { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import SwitchToggle from "react-native-switch-toggle";
+import { Image } from "expo-image";
 
 export default function HomeScreen() {
+  const [isPortrait, setIsPortrait] = useState(false);
+
   return (
     <View style={styles.container}>
-      <Text>Home Screen</Text>
+      <Text style={styles.header}>FF Panorama</Text>
+      <Image
+        source={require("../assets/images/camera_outline.png")}
+        style={[
+          styles.cameraImage,
+          { transform: [{ rotate: isPortrait ? "270deg" : "0deg" }] },
+        ]}
+      />
+      <View style={styles.switchWrapper}>
+        <Text style={styles.switchLabel}>Portrait mode</Text>
+        <SwitchToggle
+          switchOn={isPortrait}
+          onPress={() => setIsPortrait(!isPortrait)}
+          circleColorOn="#32D932"
+          circleColorOff="#f00"
+          backgroundColorOn="#ddd"
+          backgroundColorOff="#bbb"
+          containerStyle={styles.switchContainer}
+          circleStyle={styles.switchCircle}
+        />
+      </View>
     </View>
   );
 }
@@ -13,6 +38,36 @@ const styles = StyleSheet.create({
     flex: 1,
     // backgroundColor: "#fff",
     alignItems: "center",
-    justifyContent: "center",
+    justifyContent: "flex-start",
+    paddingTop: 15,
+  },
+  header: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginBottom: 10,
+  },
+  cameraImage: {
+    width: 230,
+    height: 210,
+    marginBottom: 10,
+  },
+  switchWrapper: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  switchLabel: {
+    fontSize: 18,
+    marginRight: 15,
+  },
+  switchContainer: {
+    width: 70,
+    height: 30,
+    borderRadius: 15,
+    padding: 4,
+  },
+  switchCircle: {
+    width: 25,
+    height: 25,
+    borderRadius: 15,
   },
 });
