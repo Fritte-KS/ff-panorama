2f2a1b91c9dbab036d388aa03d05c2e78a137564
Kristoffer Sundin <79krisun@skola.boras.se>
2024-10-01 17:06:59 +0200

    Updates component FieldOfView with the usage of function calculateAngleOfView

--- components/FieldOfView.tsx
+++ components/FieldOfView.tsx
@@ -1,17 +1,20 @@
 import React, { useContext } from "react";
 import { AppContext } from "../context/AppContext";
-import { View } from "react-native";
+import { Text, View } from "react-native";
 import Svg, { Path } from "react-native-svg";
+import calculateAngleOfView from "../utils/calculateAngleOfView";
 
 export default function FieldOfView() {
-  const { sliderFocal } = useContext(AppContext);
+  const { sliderFocal, isPortrait } = useContext(AppContext);
+
+  const angleOfView = calculateAngleOfView(sliderFocal, isPortrait);
 
   const radius = 150; // The radius of the circle
-  const centerX = 125;
+  const centerX = 130;
   const centerY = 152;
 
-  const startAngle = 270 - sliderFocal / 2;
-  const endAngle = 270 + sliderFocal / 2;
+  const startAngle = 270 - angleOfView / 2;
+  const endAngle = 270 + angleOfView / 2;
 
   const startRadians = (startAngle * Math.PI) / 180;
   const endRadians = (endAngle * Math.PI) / 180;
@@ -21,7 +24,7 @@ export default function FieldOfView() {
   const x2 = centerX + radius * Math.cos(endRadians);
   const y2 = centerY + radius * Math.sin(endRadians);
 
-  const largeArcFlag = sliderFocal <= 180 ? "0" : "1";
+  const largeArcFlag = angleOfView <= 180 ? "0" : "1";
   const pathData = `
     M ${centerX},${centerY}
     L ${x1},${y1} 
@@ -31,7 +34,7 @@ export default function FieldOfView() {
 
   return (
     <View style={{ justifyContent: "center", paddingBottom: 20 }}>
-      <Svg height="160" width="250">
+      <Svg height="160" width="260">
         <Path d={pathData} fill="#B7D5EE" stroke="#bbb" strokeWidth="3" />
       </Svg>
     </View>
